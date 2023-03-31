35c833c08253fb184264eeeba4b215bdf38f7a0a
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-03-31 16:11:29 -0300

    Aprendendo desenvolvimento Web Full Stack / 27 - ReactJS Intermediário | adicionando menu drower (31/03/2023)

--- 27-react-intermediario/my-app/src/partials/Header/Header.js
+++ 27-react-intermediario/my-app/src/partials/Header/Header.js
@@ -1,29 +1,72 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import {
   AppBar,
   Toolbar,
   Typography,
-  Button,
   IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
 
 import useStyles from './Header.Style';
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const history = useHistory();
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuClick = (route) => {
+    history.push(route);
+    handleToggleMenu();
+  };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          My App
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => handleToggleMenu()}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            My App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+        <List>
+          <ListItem button onClick={() => handleMenuClick('/')}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText>Home</ListItemText>
+          </ListItem>
+          <ListItem button onClick={() => handleMenuClick('/customers')}>
+            <ListItemIcon>
+              <PersonAddIcon />
+            </ListItemIcon>
+            <ListItemText>Clientes</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   );
 }
